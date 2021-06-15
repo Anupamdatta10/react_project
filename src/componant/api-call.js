e254ec84016aeed2bd1da7dcfc7d7ec5fc2b8bd2
Anupamdatta10 <anupamdatta10@gmail.com>
2021-06-15 19:15:18 +0530

    search added

--- src/componant/api-call.js
+++ src/componant/api-call.js
@@ -1,49 +1,104 @@
 import react, { Component } from "react";
 import "./counter.css";
-
+//import FormDialog from './input-modal.js'
+import  TextField  from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import NativeSelect from '@material-ui/core/NativeSelect';
+//import MenuItem from '@material-ui/core/MenuItem';
 class getdata extends Component {
-  constructor()
-  {
-      super()
-      this.state={
-          data:[],
-          url:""
-      }
-      this.showdata = this.showdata.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      url: "",
+      inputValue: ""
+   
+    };
+    this.showdata = this.showdata.bind(this);
+    this.apicall = this.apicall.bind(this);
+    // this.updateInputValue=this.updateInputValue(this);
   }
- componentDidMount()
-  {
-    fetch('https://api.sketchfab.com/v3/search?q=lungs').then((res)=>res.json()).then(json=>{
-        this.setState(
-            {
-                data:json.results.models
-            }
-        )
-    })
+
+ 
+  apicall() {
+    let { inputValue } = this.state;
+    //if(inputValue!==""){
+    fetch("https://api.sketchfab.com/v3/search?q=" + inputValue)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          data: json.results.models,
+        });
+      });
+    //}
   }
- showdata=(e)=>{
-    var {data}=this.state
-    console.log(e.target.id)
-    this.setState(
-        {url:data[e.target.id].embedUrl}
-    )
-  }
+  showdata = (e) => {
+    var { data } = this.state;
+    console.log(e.target.value);
+    if(e.target.value!=="")
+    this.setState({ url: data[e.target.value].embedUrl });
+    else
+    this.setState({ url:"" });
+  };
+  updateInputValue = (ev) => {
+    this.setState({
+      inputValue: ev.target.value,
+    });
+   
+  };
+  
   render() {
-      var {data}=this.state
-      var i=0
-    return (<react.Fragment>
+    var { data } = this.state;
+    var i = 0;
+   
+    return (
      
-      <div>
+      <react.Fragment>
+        
+        <TextField
+          id="outlined-basic"
+         
+          variant="outlined" 
+          label="searech 3d model" 
+        onChange={this.updateInputValue}
+        />
+        <Button variant="contained" color="primary" onClick={this.apicall}>
+          search
+        </Button>
+        
+        
+       
+        <NativeSelect onChange={this.showdata}>
+         <option value="">select one</option>
+          {data.map((item) => (
+            <option value={i} key={i++}  >
+              {item.name}
+        </option>
+          ))}
+          </NativeSelect>
+        
+        <div className="responsive-iframe" >
           
-              {data.map(item=>(
-                <div id={i} key={i++} onClick={this.showdata} >
-                    {item.name}
-                </div >
-              ))}
-               <iframe title="frame" src={this.state.url} className="responsive-iframe" id="api-frame" allow="autoplay; fullscreen; vr" allowvr allowFullScreen mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-      </div>
-    </react.Fragment>)
+        <iframe
+           
+            title="frame"
+            src={this.state.url}
+            className="responsive-iframe"
+            id="api-frame"
+            allow="autoplay; fullscreen; vr"
+            allowvr
+            allowFullScreen
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+          
+          </div>
+      </react.Fragment>
+    );
   }
 }
 
 export default getdata;
+/* 
+ 
+*/
